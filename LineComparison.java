39a54f7fec44e6f1df4a5b6be3bb5616d893d630
vismayatk002 <vismayatk002@gmail.com>
2021-07-26 23:01:49 +0530

    Comapared two lines

--- LineComparison.java
+++ LineComparison.java
@@ -7,15 +7,15 @@ public class LineComparison{
 
         Scanner sc= new Scanner(System.in);
         
-        //Read first co-ordinates
         System.out.println("Enter co-ordinates of First line"); 
+        //Read first co-ordinates of Line 1
         System.out.println("Enter first co-ordinates (x,y) ");  
         System.out.print("Enter x : ");  
         int x1= sc.nextInt();  
         System.out.print("Enter y : "); 
         int y1= sc.nextInt(); 
 
-        //Read second co-ordinates
+        //Read second co-ordinates of Line 1
         System.out.println("Enter second co-ordinates (x,y) ");  
         System.out.print("Enter x : ");  
         int x2= sc.nextInt();  
@@ -26,15 +26,15 @@ public class LineComparison{
         Double length1 =  Math.sqrt( Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2) );
         System.out.println("Length of first line : " + length1);
         
-
         System.out.println("Enter co-ordinates of Second line"); 
+        //Read first co-ordinates of Line 2
         System.out.println("Enter first co-ordinates (x,y) ");  
         System.out.print("Enter x : ");  
         x1= sc.nextInt();  
         System.out.print("Enter y : "); 
         y1= sc.nextInt(); 
 
-        //Read second co-ordinates
+        //Read second co-ordinates of Line 2
         System.out.println("Enter second co-ordinates (x,y) ");  
         System.out.print("Enter x : ");  
         x2= sc.nextInt();  
@@ -45,12 +45,15 @@ public class LineComparison{
         Double length2 =  Math.sqrt( Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2) );
         System.out.println("Length of second line : " + length2);
 
-        // Check equality of first and second line
-        if(length1.equals(length2)){
-            System.out.println(" Lines are equal ");
-        }
-        else{
-            System.out.println(" Lines are not equal ");
+        //Compare first and second line
+        int result =  length1.compareTo(length2);
+    
+        if(result > 0) {
+            System.out.println("First line is greater than second line");
+        } else if(result < 0) {
+            System.out.println("Second line is greater than First line");
+        } else {
+            System.out.println("Lines are equal");
         }
 
     }
